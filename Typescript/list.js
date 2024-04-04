050dc1932fbaafae6eb831ee274ceca3573c3238
preetha-2002-eng <124424334+preetha-2002-eng@users.noreply.github.com>
2024-04-04 20:17:05 +0530

    Update list.js

--- Typescript/list.js
+++ Typescript/list.js
@@ -1,26 +1,28 @@
-var numbers = [1, 2, 3, 4, 5];
-console.log("Numbers:");
-for (var _i = 0, numbers_1 = numbers; _i < numbers_1.length; _i++) {
-    var number = numbers_1[_i];
-    console.log(number);
+//Title : Duck typing
+//Author : Preetha I
+var employee_id = [101, 102, 103, 104, 105];
+console.log("Employee_ID:");
+for (var _i = 0, employee_id_1 = employee_id; _i < employee_id_1.length; _i++) {
+    var id = employee_id_1[_i];
+    console.log(id);
 }
-numbers.push(6);
-console.log("Updated Numbers:");
-for (var _a = 0, numbers_2 = numbers; _a < numbers_2.length; _a++) {
-    var number = numbers_2[_a];
-    console.log(number);
+employee_id.push(106);
+console.log("Updated Employee_ID:");
+for (var _a = 0, employee_id_2 = employee_id; _a < employee_id_2.length; _a++) {
+    var id = employee_id_2[_a];
+    console.log(id);
 }
-var fruits = ["Apple", "Banana", "Orange"];
-console.log("Fruits:");
-for (var _b = 0, fruits_1 = fruits; _b < fruits_1.length; _b++) {
-    var fruit = fruits_1[_b];
-    console.log(fruit);
+var employee_name = ["Ram", "Ravi", "Kumar"];
+console.log("Employee_Name:");
+for (var _b = 0, employee_name_1 = employee_name; _b < employee_name_1.length; _b++) {
+    var name_1 = employee_name_1[_b];
+    console.log(name_1);
 }
-console.log("First fruit:", fruits[0]);
-console.log("Second fruit:", fruits[1]);
-fruits[1] = "Grapes";
-console.log("Updated Fruits:");
-for (var _c = 0, fruits_2 = fruits; _c < fruits_2.length; _c++) {
-    var fruit = fruits_2[_c];
-    console.log(fruit);
+console.log("First employee:", employee_name[0]);
+console.log("Second employee:", employee_name[1]);
+employee_name[1] = "Reka";
+console.log("Updated Employee_Name:");
+for (var _c = 0, employee_name_2 = employee_name; _c < employee_name_2.length; _c++) {
+    var name_2 = employee_name_2[_c];
+    console.log(name_2);
 }
